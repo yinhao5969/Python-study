2f0333518a63e2bd86e9284a2b2fc7cd28ae4604
yinhao5969 <yinhao5969@icloud.com>
2019-11-29 23:24:41 +0800

    vs commit test

--- alien_attack/setting.py
+++ alien_attack/setting.py
@@ -4,5 +4,5 @@ class Setting():
         """initiall game settings"""
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (200, 200, 200)
+        self.bg_color = (200, 200, 199)
         
